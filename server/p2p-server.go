58bef907793c3734da63cb8ca7ed5f3955abe10e
Ryan Kline <kliner@mail.gvsu.edu>
2023-04-04 14:18:41 -0400

    Initialize Server

--- server/p2p-server.go
+++ server/p2p-server.go
@@ -12,9 +12,46 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	SERVER_HOST = "localhost"
+	SERVER_PORT = "8636"
+	SERVER_TYPE = "tcp"
 )
 
 func main() {
+	fmt.Println("Server Running...")
+	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
 
-	fmt.Println("Hello World!")
+	defer func(server net.Listener) {
+		err := server.Close()
+		if err != nil {
+			fmt.Println("Cannot close server:", err.Error())
+			os.Exit(1)
+		}
+	}(server)
+
+	fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+	fmt.Println("Waiting for client...")
+
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Client connected")
+		go processClient(connection)
+	}
+}
+
+func processClient(connection net.Conn) {
+	fmt.Println("Implement Me!")
 }
