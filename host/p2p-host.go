0e618009e10ebd5f05bf2a9c7a14cf6b62cfd886
Ryan Kline <kliner@mail.gvsu.edu>
2023-04-06 11:16:41 -0400

    Add

--- host/p2p-host.go
+++ host/p2p-host.go
@@ -30,6 +30,8 @@ const (
 
 var SERVER_PORT = generatePortNumber()
 
+/* =============== Host FTP Server/Client =================== */
+
 func ftpServer(syncUp chan bool) {
 	/*
 	   Starts up server using the host, port, and
@@ -60,11 +62,11 @@ func ftpServer(syncUp chan bool) {
 			os.Exit(1)
 		}
 		fmt.Println("Client connected")
-		go processClient(connection)
+		go processFTPClient(connection)
 	}
 }
 
-func processClient(connection net.Conn) {
+func processFTPClient(connection net.Conn) {
 	/*
 		Listens for commands sent from the client. If a command requires a data transfer, the server connects to
 		the data line hosted by the client before calling the handleDataTransfer() function that takes appropriate actions
@@ -141,113 +143,87 @@ func ftpClient(syncUp chan bool) {
 		prompted to send another command. This loop continues until the client sends the "QUIT" command.
 	*/
 	var (
-		command        string
-		connectPattern = `^CONNECT ([a-zA-Z0-9\-\.]+:[0-9]+)$`
-		DATA_PORT      = generatePortNumber()
+		command   string
+		DATA_PORT = generatePortNumber()
 	)
 
 	// Waits for Server to be initialized before initializing client
 	<-syncUp
 	for {
-		fmt.Println("Connect to a server:")
-
-		scanner := bufio.NewScanner(os.Stdin)
-
-		if scanner.Scan() {
-			command = scanner.Text()
+		control := connectToServer()
+		if control == nil {
+			continue
 		}
+		// Send Host/Port info for data connection
+		_, err := control.Write([]byte(SERVER_HOST + ":" + DATA_PORT))
+		if err != nil {
+			fmt.Println("Unable to write to server:", err.Error())
+			return
+		}
+		// Interact with the server via commands
+		for {
+			scanner := bufio.NewScanner(os.Stdin)
+			fmt.Println("Enter a command:")
 
-		if matched, err := regexp.MatchString(connectPattern, command); err == nil && matched {
-			splitCommand := strings.Split(command, " ")
-			hostAndPort := strings.Split(splitCommand[1], ":")
-			host := hostAndPort[0]
-			port := hostAndPort[1]
-
-			// Control Connection
-			control, err := net.Dial(SERVER_TYPE, host+":"+port)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			} else {
-				fmt.Println(fmt.Sprintf("[Control] Connected to %s:%s", host, port))
+			if scanner.Scan() {
+				command = scanner.Text()
 			}
 
-			// Send Host/Port info for data connection
-			_, err = control.Write([]byte(SERVER_HOST + ":" + DATA_PORT))
-			if err != nil {
-				fmt.Println("Unable to write to server:", err.Error())
-				return
-			}
-			// Interact with the server via commands
-			for {
-				scanner := bufio.NewScanner(os.Stdin)
-				fmt.Println("Enter a command:")
-
-				if scanner.Scan() {
-					command = scanner.Text()
-				}
-
-				if command != "QUIT" {
-					if isValidCommand(command) {
-						fmt.Println("[Data] Port Running on " + SERVER_HOST + ":" + DATA_PORT)
-						server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+DATA_PORT)
-						if err != nil {
-							fmt.Println("[Data] Error listening:", err.Error())
-							return
-						}
-
-						_, err = control.Write([]byte(command))
-						if err != nil {
-							fmt.Println("[Control] Error writing:", err.Error())
-							return
-						}
-
-						dataConnection, err := server.Accept()
-						if err != nil {
-							fmt.Println("[Data] Error accepting client:", err.Error())
-							return
-						}
-
-						err = retrieveData(command, dataConnection)
-						if err != nil {
-							fmt.Println("[Data] Error in data transfer:", err.Error())
-							return
-						}
-
-						fmt.Println("[Data] Port Closing")
-						err = dataConnection.Close()
-						if err != nil {
-							fmt.Println("[Data] Error closing dataConnection to client:", err.Error())
-							return
-						}
-
-						err = server.Close()
-						if err != nil {
-							fmt.Println("[Data] Error closing server:", err.Error())
-							return
-						}
+			if command != "QUIT" {
+				if isValidCommand(command) {
+					fmt.Println("[Data] Port Running on " + SERVER_HOST + ":" + DATA_PORT)
+					server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+DATA_PORT)
+					if err != nil {
+						fmt.Println("[Data] Error listening:", err.Error())
+						return
 					}
-				} else if command == "QUIT" {
-					_, err := control.Write([]byte(command))
+
+					_, err = control.Write([]byte(command))
 					if err != nil {
 						fmt.Println("[Control] Error writing:", err.Error())
 						return
 					}
-					break
-				} else {
-					fmt.Println("Invalid command. Try again")
+
+					dataConnection, err := server.Accept()
+					if err != nil {
+						fmt.Println("[Data] Error accepting client:", err.Error())
+						return
+					}
+
+					err = retrieveData(command, dataConnection)
+					if err != nil {
+						fmt.Println("[Data] Error in data transfer:", err.Error())
+						return
+					}
+
+					fmt.Println("[Data] Port Closing")
+					err = dataConnection.Close()
+					if err != nil {
+						fmt.Println("[Data] Error closing dataConnection to client:", err.Error())
+						return
+					}
+
+					err = server.Close()
+					if err != nil {
+						fmt.Println("[Data] Error closing server:", err.Error())
+						return
+					}
 				}
+			} else if command == "QUIT" {
+				_, err := control.Write([]byte(command))
+				if err != nil {
+					fmt.Println("[Control] Error writing:", err.Error())
+					return
+				}
+				break
+			} else {
+				fmt.Println("Invalid command. Try again")
 			}
-			err = control.Close()
-			if err != nil {
-				fmt.Println("[Control] Error closing connection to server:", err.Error())
-				return
-			}
-		} else if command == "exit" {
-			os.Exit(0)
-		} else {
-			fmt.Println("Invalid Command. Use `CONNECT <server name/IP address> <server port>` to connect to " +
-				"a server")
+		}
+		err = control.Close()
+		if err != nil {
+			fmt.Println("[Control] Error closing connection to server:", err.Error())
+			return
 		}
 	}
 }
@@ -394,6 +370,43 @@ func generatePortNumber() string {
 	port := rand.Intn(5000) + 5000
 
 	return strconv.Itoa(port)
+}
+
+func connectToServer() net.Conn {
+	var command string
+	connectPattern := `^CONNECT ([a-zA-Z0-9\-\.]+:[0-9]+)$`
+
+	fmt.Println("Connect to a server:")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if scanner.Scan() {
+		command = scanner.Text()
+	}
+
+	if matched, err := regexp.MatchString(connectPattern, command); err == nil && matched {
+		splitCommand := strings.Split(command, " ")
+		hostAndPort := strings.Split(splitCommand[1], ":")
+		host := hostAndPort[0]
+		port := hostAndPort[1]
+
+		connection, err := net.Dial(SERVER_TYPE, host+":"+port)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(fmt.Sprintf("[Control] Connected to %s:%s", host, port))
+			return connection
+		}
+	} else if command == "exit" {
+		os.Exit(0)
+	} else {
+		fmt.Println("Invalid Command. Use `CONNECT <server name/IP address>:<server port>` to connect to a server")
+	}
+	return nil
+}
+
+/* =============== Host to Central Server ================== */
+func registerWithServer() {
+	fmt.Println("Work in progress")
 }
 
 func main() {
